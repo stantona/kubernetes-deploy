29b9d60a4543156658e1ec87dee2ad43e0aafd87
Katrina Verey <katrina.verey@shopify.com>
2018-11-23 14:29:20 -0500

    [ci-skip] Add script to simulate the watch part of a deploy

--- lib/kubernetes-deploy/resource_discovery.rb
+++ lib/kubernetes-deploy/resource_discovery.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+require 'kubernetes-deploy/template_discovery'
 module KubernetesDeploy
   class ResourceDiscovery
-    def initialize(namespace:, context:, logger:, namespace_tags:)
+    def initialize(namespace:, context:, logger:, namespace_tags: [])
       @namespace = namespace
       @context = context
       @logger = logger
@@ -14,6 +15,15 @@ module KubernetesDeploy
         CustomResourceDefinition.new(namespace: @namespace, context: @context, logger: @logger,
           definition: cr_def, statsd_tags: @namespace_tags)
       end
+    end
+
+    def from_templates(template_dir, renderer)
+      template_files = TemplateDiscovery.new(template_dir).templates
+      resources = []
+      renderer.render_files(template_files).each do |filename, definitions|
+        definitions.each { |r_def| resources << build_resource(r_def, filename) }
+      end
+      resources
     end
 
     private
@@ -29,6 +39,19 @@ module KubernetesDeploy
 
     def kubectl
       @kubectl ||= Kubectl.new(namespace: @namespace, context: @context, logger: @logger, log_failure_by_default: true)
+    end
+
+    def build_resource(definition, filename)
+      KubernetesResource.build(
+        namespace: @namespace,
+        context: @context,
+        logger: @logger,
+        definition: definition,
+        statsd_tags: @namespace_tags
+      )
+    rescue InvalidTemplateError => e
+      e.filename ||= filename
+      raise e
     end
   end
 end
