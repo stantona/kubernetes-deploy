1504549e74c7d34d27fdaf8950a65b154639ab99
Katrina Verey <katrina.verey@shopify.com>
2018-11-26 19:08:53 -0500

    Use Kubeclient in ResourceDiscovery

--- lib/kubernetes-deploy/resource_discovery.rb
+++ lib/kubernetes-deploy/resource_discovery.rb
@@ -2,19 +2,29 @@
 
 module KubernetesDeploy
   class ResourceDiscovery
+    include KubeclientBuilder
+
     def initialize(namespace:, context:, logger:, namespace_tags:)
       @namespace = namespace
       @context = context
       @logger = logger
       @namespace_tags = namespace_tags
-      @cache = ResourceCache.new(namespace, context, logger)
     end
 
     def crds
-      @crds ||= @cache.get_all(CustomResourceDefinition.kind).map do |r_def|
-        CustomResourceDefinition.new(namespace: @namespace, context: @context, logger: @logger,
-          definition: r_def, statsd_tags: @namespace_tags)
+      @crds ||= begin
+        raw = JSON.parse(crd_client.get_custom_resource_definitions(as: :raw))
+        raw["items"].map do |r_def|
+          CustomResourceDefinition.new(namespace: @namespace, context: @context, logger: @logger,
+            definition: r_def, statsd_tags: @namespace_tags)
+        end
       end
+    end
+
+    private
+
+    def crd_client
+      @crd_client ||= build_apiextensions_v1beta1_kubeclient(@context)
     end
   end
 end
