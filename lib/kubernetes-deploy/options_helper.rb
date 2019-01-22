d674910dfd34dec37523cecc73f533fbe55cc72c
timothysmith0609 <tim.smith@shopify.com>
2019-01-22 12:23:28 -0500

    Without lambdas

--- lib/kubernetes-deploy/options_helper.rb
+++ lib/kubernetes-deploy/options_helper.rb
@@ -2,11 +2,22 @@
 
 module KubernetesDeploy
   module OptionsHelper
-    def self.default_and_check_template_dir(template_dir)
+    def self.default_and_check_template_dir(template_dir, use_stdin: false)
+      if use_stdin
+        if $stdin.tty?
+          puts "Nothing to read from $stdin"
+          exit(1)
+        end
+
+        template_dir = Dir.mktmpdir("kubernetes-deploy")
+        input = $stdin.readlines.join("")
+        File.open("#{File.expand_path(template_dir)}/templates.yml.erb", 'w+') { |f| f.print(input) }
+        return template_dir
+      end
+
       if !template_dir && ENV.key?("ENVIRONMENT")
         template_dir = "config/deploy/#{ENV['ENVIRONMENT']}"
       end
-
       if !template_dir || template_dir.empty?
         puts "Template directory is unknown. " \
           "Either specify --template-dir argument or set $ENVIRONMENT to use config/deploy/$ENVIRONMENT " \
