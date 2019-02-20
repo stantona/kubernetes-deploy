1ae441ee59b86e4a0399b2b57943ab5a9b396f00
timothysmith0609 <tim.smith@shopify.com>
2019-02-20 09:18:38 -0500

    Different approach with yielded deploy dir
    prevent hanging waiting for stream. Tidyup
    
    renames
    
    More flexible args handling
    
    remove debug gem
    
    OptionsHelper as singleton class
    
    cop
    
    most important method at top of file
    
    Tests, OptionsError, refactors
    
    Automatic corrections for lib/kubernetes-deploy/options_helper.rb
    
    Automatic corrections for test/unit/kubernetes-deploy/options_helper_test.rb
    
    fix test
    
    police
    
    Don't use temp file if only using a single template dir and no additional input
    
    tidier

--- lib/kubernetes-deploy/options_helper.rb
+++ lib/kubernetes-deploy/options_helper.rb
@@ -2,36 +2,58 @@
 
 module KubernetesDeploy
   module OptionsHelper
-    def self.default_and_check_template_dir(template_dir, use_stdin: false)
-      if use_stdin
-        if $stdin.tty?
-          puts "Nothing to read from $stdin"
-          exit(1)
+    class OptionsError < StandardError; end
+
+    STDIN_TEMP_FILE = "from_stdin.yml.erb"
+    class << self
+      def with_consolidated_template_dir(template_dirs)
+        if template_dirs.length <= 1 && ARGV.empty? && $stdin.tty?
+          yield default_template_dir(template_dirs.first)
+        else
+          Dir.mktmpdir("kubernetes-deploy") do |dir|
+            populate_temp_dir(temp_dir: dir, template_dirs: template_dirs)
+            yield dir
+          end
+        end
+      end
+
+      def revision_from_environment
+        ENV.fetch('REVISION') do
+          raise OptionsError, "ENV['REVISION'] is missing. Please specify the commit SHA"
+        end
+      end
+
+      private
+
+      def default_template_dir(template_dir)
+        if ENV.key?("ENVIRONMENT")
+          template_dir = "config/deploy/#{ENV['ENVIRONMENT']}"
         end
 
-        template_dir = Dir.mktmpdir("kubernetes-deploy")
-        input = $stdin.readlines.join("")
-        File.open("#{File.expand_path(template_dir)}/templates.yml.erb", 'w+') { |f| f.print(input) }
-        return template_dir
+        if !template_dir || template_dir.empty?
+          raise OptionsError, "Template directory is unknown. " \
+            "Either specify --template-dir argument or set $ENVIRONMENT to use config/deploy/$ENVIRONMENT " \
+            "as a default path."
+        end
+
+        template_dir
       end
 
-      if !template_dir && ENV.key?("ENVIRONMENT")
-        template_dir = "config/deploy/#{ENV['ENVIRONMENT']}"
-      end
-      if !template_dir || template_dir.empty?
-        puts "Template directory is unknown. " \
-          "Either specify --template-dir argument or set $ENVIRONMENT to use config/deploy/$ENVIRONMENT " \
-        + "as a default path."
-        exit(1)
-      end
+      def populate_temp_dir(temp_dir:, template_dirs:)
+        File.open("#{temp_dir}/#{STDIN_TEMP_FILE}", 'w+') { |f| f.print(ARGF.read) } unless $stdin.tty?
 
-      template_dir
-    end
-
-    def self.revision_from_environment
-      ENV.fetch('REVISION') do
-        puts "ENV['REVISION'] is missing. Please specify the commit SHA"
-        exit 1
+        template_dirs.each do |template_dir|
+          template_dir = File.expand_path(template_dir)
+          templates = Dir.entries(template_dir).reject { |f| f =~ /^\.{1,2}$/ }
+          templates.each do |template|
+            next if File.directory?(File.expand_path("#{template_dir}/#{template}"))
+            File.open("#{temp_dir}/#{template_dir.tr('/', '_')}_#{template}", 'w+') do |f|
+              f.print(File.read("#{template_dir}/#{template}"))
+            end
+          end
+        end
+      rescue IOError, Errno::ENOENT => e
+        raise OptionsError, e.message
       end
     end
   end
